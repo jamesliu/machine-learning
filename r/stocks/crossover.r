dfb6b3c4a50f658ac170a1fd1bf5bb5beb311fe0
James Liu <james.ch.liu@gmail.com>
2012-11-23 22:08:26 -0800

    -

--- r/stocks/crossover.r
+++ r/stocks/crossover.r
@@ -4,4 +4,3 @@ signals = cbind(signals, signals[,1] - signals[,2])
 signals = cbind (signals,diff(sign(signals[,3])))
 Cross_Date = rownames(as.matrix(signals))[which (signals[,4] != 0)]
 addTA(signals[Cross_Date,1], on=1, type = "p", col=5, pch=24)
-r
