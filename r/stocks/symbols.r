743ca1f8370652e31ead1aacf3615b3bc0844162
James Liu <james.ch.liu@gmail.com>
2012-12-03 19:45:22 -0800

    -

--- r/stocks/symbols.r
+++ r/stocks/symbols.r
@@ -1,5 +1,10 @@
 library(qmao)
+library(rjson)
 ec <- getEarningsCalendar(from="2011-01-01", to="2012-07-01") #this may take a while
 s <- unique(ec$Symbol)
 
+sink("symbols.txt")
+cat(toJSON(s))
+sink()
+file.show("symbols.txt")
 
