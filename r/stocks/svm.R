a456881a8c80135159e84af88e6ec94469c73c5e
James Liu <james.ch.liu@gmail.com>
2012-12-05 23:00:13 -0800

    -

---
+++ r/stocks/svm.R
@@ -0,0 +1,6 @@
+library(e1071)
+sv <- svm(Tform, Tdata.train[1:100, ], gamma = 0.001, cost = 100)
+s.preds <- predict(sv, Tdata.train[101:200, ])
+sigs.svm <- trading.signals(s.preds, 0.1, -0.1)
+true.sigs <- trading.signals(Tdata.train[101:200, "indicator.DDD"], 0.1, -0.1)
+sigs.PR(sigs.svm, true.sigs)
