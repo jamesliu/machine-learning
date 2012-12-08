c178a3a699ca7e903d0374123722e13e11d2bd1e
James Liu <james.ch.liu@gmail.com>
2012-12-07 19:40:51 -0800

    -

---
+++ r/stocks/montecarlo.R
@@ -0,0 +1,58 @@
+MC.svmR <- function(form, train, test, b.t = 0.1, s.t = -0.1,
+                    ...) {
+  require(e1071)
+  t <- svm(form, train, ...)
+  p <- predict(t, test)
+  trading.signals(p, b.t, s.t)
+}
+
+MC.svmC <- function(form, train, test, b.t = 0.1, s.t = -0.1,+ ...) {
+  require(e1071)+ tgtName <- all.vars(form)[1]
+  train[, tgtName] <- trading.signals(train[, tgtName],+ b.t, s.t)
+  t <- svm(form, train, ...)+ p <- predict(t, test)
+  factor(p, levels = c("s", "h", "b"))
+}
+
+MC.nnetR <- function(form, train, test, b.t = 0.1, s.t = -0.1,
+                     ...) {
+  require(nnet)+ t <- nnet(form, train, ...)
+  p <- predict(t, test)
+  trading.signals(p, b.t, s.t)
+}
+
+MC.nnetC <- function(form, train, test, b.t = 0.1, s.t = -0.1,
+                     ...) {
+  require(nnet)
+  tgtName <- all.vars(form)[1]
+  train[, tgtName] <- trading.signals(train[, tgtName],
+                                      b.t, s.t)
+  t <- nnet(form, train, ...)
+  p <- predict(t, test, type = "class")
+  factor(p, levels = c("s", "h", "b"))
+}
+
+MC.earth <- function(form, train, test, b.t = 0.1, s.t = -0.1,
+                     ...) {
+  require(earth)
+  t <- earth(form, train, ...)
+  p <- predict(t, test)
+  trading.signals(p, b.t, s.t)
+}
+
+single <- function(form, train, test, learner, policy.func,
+                   ...) {
+  p <- do.call(paste("MC", learner, sep = "."), list(form,+ train, test, ...))
+  eval.stats(form, train, test, p, policy.func = policy.func)
+}
+
+slide <- function(form, train, test, learner, relearn.step,
+                  policy.func, ...) {
+  real.learner <- learner(paste("MC", learner, sep = "."),
+                          pars = list(...))
+  p <- slidingWindowTest(real.learner, form, train, test,
+                         relearn.step)
+  p <- factor(p, levels = 1:3, labels = c("s", "h", "b"))
+  eval.stats(form, train, test, p, policy.func = policy.func)
+}
+
+grow <- function(form, train, test, learner, relearn.step,+ policy.func, ...) { + real.learner <- learner(paste("MC", learner, sep = "."),+ pars = list(...))+ p <- growingWindowTest(real.learner, form, train, test,+ relearn.step)+ p <- factor(p, levels = 1:3, labels = c("s", "h", "b"))+ eval.stats(form, train, test, p, policy.func = policy.func)
---
+++ r/stocks/trainSVM.R
@@ -0,0 +1,18 @@
+# Train and test
+start <- 1
+len.tr <- 100
+len.ts <- 50
+tr <- start:(start+len.tr-1)
+ts <- (start+len.tr):(start+len.tr+len.ts-1)
+# getting the quotes for the testing period
+
+date <- rownames(Tdata.train[start+len.tr,])
+market <- DDD[paste(date, '/', sep='')][1:len.ts]
+colnames(market) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted")
+# learning the model and obtaining its signal predictions
+library(e1071)
+s <- svm(Tform,Tdata.train[tr,],cost=10,gamma=0.01)
+p <- predict(s,Tdata.train[ts,])
+sig <- trading.signals(p,0.1,-0.1)
+# now using the simulated trader
+t1 <- trading.simulator(market,sig,'policy.1',list(exp.prof=0.05,bet=0.2,hold.time=30))
